99a977ff5cbd32e6bf11a8ed2de17366b425b013
Remy Wang <remywang@protonmail.com>
2017-12-11 22:26:09 -0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# align
+# Dynamic programming algorithms for sequence alignment
+
+## Note
+
+I intentionally chose Haskell as programming language for this project, 
+to showcase its power in implementing DP algorithms. Haskell implements
+lazy evaluation, which automatically results in memoization of computed
+values, therefore the programmer gets dynamic programming for free. 
+
+Follow https://docs.haskellstack.org/en/stable/README/ to install Haskell, 
+then compile and run with `bash run.sh`. The result is stored in `out`. 
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,37 +7,48 @@ import Debug.Trace
 
 type Tab = [(String, [(String, Int)])]
 
+-- configurations
+
+-- number of permutations for p values
 mutants :: Int
 mutants = 1000
 
+-- gap penalty
 gapd :: Int
 gapd = 4
 
+-- gap penalty for smith waterman
 gapsw :: Int
 gapsw = 8
+
+-- NW algorithm
 
 f :: Int -> [String] -> [String] -> Tab -> Int -> Int -> (Int, [(String, String)])
 f d x y t = trace ("aligning " ++ concat (take 10 y) ++ "...") f_
   where f_ = Memo.memo2 Memo.integral Memo.integral f'
           where
+            -- initialization
             f' 0 0 = (0, [])
             f' 0 j = (- j * d, [])
             f' i 0 = (- i * d, [])
-            f' i j = let (fd, ad) = f_ (i-1) (j-1)
-                         (fl, al) = f_ (i-1) j
-                         (fu, au) = f_ i (j-1)
-                         (a, b) = (x !! (i-1), y !! (j-1))
+            -- inductive step
+            f' i j = let (fd, ad) = f_ (i-1) (j-1) -- diagonal
+                         (fl, al) = f_ (i-1) j     -- left
+                         (fu, au) = f_ i (j-1)     -- right
+                         (a, b) = (x !! (i-1), y !! (j-1)) -- 0-index
                          better (fx, _) (fy, _) = compare fx fy
+            -- pick best
                      in maximumBy better
                         [ (fd + score (x !! (i-1)) (y !! (j-1)) t, (a, b):ad)
                         , (fl - d, (a, "_"):al)
                         , (fu - d, ("_", b):au)]
 
+-- smith waterman version
 fsw :: Int -> [String] -> [String] -> Tab -> Int -> Int -> ((Int, [(String, String)]), (Int, [(String, String)]))
 fsw d x y t = trace ("aligning " ++ concat (take 10 y) ++ "...") f_
   where f_ = Memo.memo2 Memo.integral Memo.integral f'
           where
-            f' 0 _ = ((0, []), (0, []))
+            f' 0 _ = ((0, []), (0, [])) -- second result is best so far
             f' _ 0 = ((0, []), (0, []))
             f' i j = let ((fd, ad), bestd) = f_ (i-1) (j-1)
                          ((fl, al), bestl) = f_ (i-1) j
@@ -53,39 +64,49 @@ fsw d x y t = trace ("aligning " ++ concat (take 10 y) ++ "...") f_
                          best = maximumBy better (ans : bests)
                      in (ans, best)
 
-outter :: [a] -> [a] -> [(a, a)]
-outter xs = concatMap (\y -> zip (repeat y) xs)
-
 someFunc :: IO ()
 someFunc = do
+  -- read BLOSUM62 matrix
   fc <- readFile "BLOSUM62.txt"
   let cs:rs = map words $ lines fc
-      tab = map (match cs) rs
-      x = tolist "DEADLY"
+      tab = map (tally cs) rs
+  -- aligning DEADLY and DDGEARLYK
+  print "aligning DEADLY and DDGEARLYK"
+  let x = tolist "DEADLY"
       y = tolist "DDGEARLYK"
       dead = f gapd y x tab (length y) (length x)
+  print "alignment result"
   printAlignment dead
   deadp <- pval y x tab
+  print "deadly p value"
   print deadp
-  let (_, try) = f gapd (tolist hbb_human) (tolist insl3_human) tab (length hbb_human) (length insl3_human)
-      (a, b) = unzip . reverse $ try
-  print $ concat a
-  print $ concat b
+  -- aligning proteins
+  print "aligning proteins"
   let res = map (\(l, r) -> f gapd (tolist l) (tolist r) tab (length l) (length r)) pairs
+  print "alignment results"
   _ <- mapM printAlignment res
   pvals <- mapM (\(l, r) -> pval (tolist l) (tolist r) tab) pairs
+  print "protein p values"
   print pvals
+  -- local alignment
+  print "local alignment"
   let locx = tolist "SRGMIEVGNQWT"
       locy = tolist "RGMVVGRW"
       (_, ans) = fsw gapsw locx locy tab (length locx) (length locy)
   printAlignment ans
   print "DONE"
 
+-- p value of sequence alignment
+
 pval :: [String] -> [String] -> Tab -> IO Double
 pval xs ys tab = do
+  -- get permutations
   perms <- replicateM mutants $ shuffleM ys
+  -- align permutations
   let (sperms, _) = unzip $ map (\p -> f gapd xs p tab (length xs) (length p)) perms
+  -- align original
       (sal, _) = f gapd xs ys tab (length xs) (length ys)
+  -- percentile of original alignment
   return $ fromIntegral (length (filter (> sal) sperms)) / fromIntegral (length sperms)
 
 printAlignment :: (Int, [(String, String)]) -> IO()
@@ -98,17 +119,23 @@ printAlignment (s, a) = do
 tolist :: String -> [String]
 tolist = map (: [])
 
-match :: [String] -> [String] -> (String, [(String, Int)])
-match cs (a:ss) =
+-- read a score entry from the input matrix file
+
+tally :: [String] -> [String] -> (String, [(String, Int)])
+tally cs (a:ss) =
   let ns = map read ss :: [Int]
    in (a, zip cs ns)
-match _ _ = undefined
+tally _ _ = undefined
+
+-- lookup a match from the scoring matrix
 
 score :: String -> String -> [(String, [(String, Int)])] -> Int
 score x y t =
   let Just r = lookup y t
       Just n = lookup x r
   in n
+
+-- protein pairs to align
 
 pairs :: [(String, String)]
 pairs = zip (repeat hbb_human) [ hbb_pantr
@@ -117,6 +144,8 @@ pairs = zip (repeat hbb_human) [ hbb_pantr
                                , q802a3_takru
                                , q540f0_vigun
                                , insl3_human]
+
+-- proteins
 
 hbb_human = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH"
 hbb_pantr = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH"
